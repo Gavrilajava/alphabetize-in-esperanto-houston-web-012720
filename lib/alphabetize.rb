1ea19818134f4432ee9e8039acf490fd04a755b6
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-02-01 13:24:45 -0600

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,21 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  arr.sort{ |a,b|
+    sr = 0
+    i = 0
+    while i < [a,b].max_by { |x| x.length }.length do
+      sr = ESPERANTO_ALPHABET.index(a[i]) <=> ESPERANTO_ALPHABET.index(b[i])
+      if sr != 0
+        break
+      else 
+        i += 1
+      end
+    end
+    sr
+    
+    
+  }
 end
+
